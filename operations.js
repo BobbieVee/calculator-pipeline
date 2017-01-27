b96509e47b3ee5a049818abe34e5b845642a35c3
Robert van der Mark <robert.vandermark@gmail.com>
2017-01-27 11:48:33 -0500

    es6-ed it

--- operations.js
+++ operations.js
@@ -1,28 +1,20 @@
 const operations = {
 	total: 0,
-	set: function(operand){
-		this.total = operand;
-	}, 
-	add: function(operand){
-		this.total += operand;
-	}, 
-	multiply: function(operand){
-		this.total *= operand;
-	},
-	negate: function(){
-		this.total = -this.total;
-	}, 
-	divide: function(operand){
-		this.total /= operand;
-	},
-	run: function(op, operand){
-		if (op === 'add') this.add(operand);
-		else if(op === 'set') this.set(operand);
-		else if (op === 'multiply')this.multiply(operand);
-		else if (op === 'negate') this.negate();
-		else if (op === 'divide') this.divide(operand);
+	// set: function(operand){
+	// 	this.total = operand;
+	// }, 
+	set: (operand) => operations.total = operand,
+	add: (operand) => operations.total += operand,
+	multiply: (operand) => operations.total *= operand,
+	negate: () => operations.total = -operations.total, 
+	divide: (operand) => operations.total /= operand,
+	run: (op, operand) => {
+		if (op === 'add') operations.add(operand);
+		else if(op === 'set') operations.set(operand);
+		else if (op === 'multiply')operations.multiply(operand);
+		else if (op === 'negate') operations.negate();
+		else if (op === 'divide') operations.divide(operand);
 	}
 };
-	
 
 module.exports = operations;
